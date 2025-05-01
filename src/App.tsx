3077b7db8d1fb7635d25a93fc333adb9f7fe94ba
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2025-05-01 04:31:53 +0100

    setter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import IOSCameraDebug from "./components/IOSCameraDebug";
 import LiveKitWrapper from "./components/LiveKitWrapper";
 import  ErrorBoundary  from "./ErrorBoundary";
 import { isIOSBrowser } from "./utils/ios-direct-fix";
-// import SolanaWalletSetter from "./components/SolanaWalletSetter";
+import SolanaWalletSetter from "./components/SolanaWalletSetter";
 // import TestingHooks from "./components/TestingHooks";
 // import CallControlz from "./components/call-controlz"; ki5-kcd-n8v
 // import WebSocketTest from "./components/websockettest"; xu0-7sx-hoz     // "build": "tsup",
@@ -13,7 +13,7 @@ function App() {
   return (
     <ErrorBoundary>
       <LiveKitWrapper>
-        {/* <SolanaWalletSetter /> */}
+        <SolanaWalletSetter />
         {showDebug && <IOSCameraDebug />}
         <VidbloqProvider
           apiKey="sk_5fa927d2ad021016ae36b2656fbf8085"
