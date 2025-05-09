7798b9248457376a40383888854cc6d3fcf59a93
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2025-05-09 17:40:01 +0100

    endpoint changed

--- src/utils/base-api.ts
+++ src/utils/base-api.ts
@@ -1,7 +1,7 @@
 // export const baseApi = "https://server.thestreamlink.com"
-export const baseApi = "http://localhost:8001"
+// export const baseApi = "http://localhost:8001"
 
-// export const baseApi = "https://probable-octo-umbrella-production.up.railway.app"
+export const baseApi = "https://probable-octo-umbrella-production.up.railway.app"
 
 
 // export const websocketUrl = baseApi.replace('http', 'ws') + '/ws';
