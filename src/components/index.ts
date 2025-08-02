702443588ad0e29f983567fb16b09cff33ed2d48
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2025-08-02 10:07:59 +0100

    updated index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,10 +4,10 @@ export { default as StreamView } from "./stream-view";
 export { default as WalletAdapterBridge } from "./wallet-adapter-bridge";
 export * from "./audio-video-controls"
 export { default as BaseCallControls } from "./base-call-controls";
-export { default as UserView } from "./user-view";
-export { default as Prejoin } from "./prejoin";
-export { default as CallControls } from "./call-controls";
-export { default as ParticipantView } from "./participant";
-export { default as ParticipantControls } from "./participant-controls";
-export { default as ReactionsBar } from "./reactions"
+// export { default as UserView } from "./ui-remove/user-view";
+// export { default as Prejoin } from "./prejoin";
+// export { default as CallControls } from "./ui-remove/call-controls-old";
+// export { default as ParticipantView } from "./ui-remove/participant";
+// export { default as ParticipantControls } from "./participant-controls";
+// export { default as ReactionsBar } from "./reactions"
 export * from "./track-controls"
